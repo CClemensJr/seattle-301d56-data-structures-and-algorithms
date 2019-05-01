8975889d2398186bd744414eb704286b740f1d07
Charles Clemens <cclemensjr@gmail.com>
2019-04-30 17:34:38 -0700

    Passed challenge 4

--- map/challenges-07.test.js
+++ map/challenges-07.test.js
@@ -52,9 +52,7 @@ Read the MDN documentation on String.charCodeAt() if necessary.
 For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
-const charCode = (arr) => {
-  // Solution code here...
-};
+const charCode = (arr) => arr.map((char) => char.charCodeAt());
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
